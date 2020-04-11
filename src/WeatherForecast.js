30e69dc25d89f8b39ec343b0be7931c343781184
Mariana Silva <marianamvsilva@gmail.com>
2020-04-11 16:21:12 +0100

    Added UseState Forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,36 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function Forecast() {
-  return (
-    <div className="forecast border bg-light">
-      <div className="row">
-        <div className="col-sm" id="day__block">
-          <p>Tomorrow</p>
-          <p>0ºC</p>
-          <img id="day__block-icon" alt="mariana" />
-        </div>
-        <div className="col-sm" id="day__block">
-          <p>Tomorrow</p>
-          <p>0ºC</p>
-          <img id="day__block-icon" alt="mariana" />
-        </div>
-        <div className="col-sm" id="day__block">
-          <p>Tomorrow</p>
-          <p>0ºC</p>
-          <img id="day__block-icon" alt="mariana" />
-        </div>
-        <div className="col-sm" id="day__block">
-          <p>Tomorrow</p>
-          <p>0ºC</p>
-          <img id="day__block-icon" alt="mariana" />
-        </div>
-        <div className="col-sm" id="day__block">
-          <p>Tomorrow</p>
-          <p>0ºC</p>
-          <img id="day__block-icon" alt="mariana" />
-        </div>
-      </div>
-    </div>
-  );
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+  function handleForecastResponse(response) {
+    setLoaded(true);
+    setForecast(response.data);
+  }
+
+  if (loaded) {
+    console.log(forecast);
+    return "loaded";
+  } else {
+    let apiKey = "020587e6f2a6601e01854941fcf9435b";
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(handleForecastResponse);
+    return null;
+  }
 }
