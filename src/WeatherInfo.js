3450927c2c114e840c82934dad70d26d0050a059
Mariana Silva <marianamvsilva@gmail.com>
2020-04-13 14:59:51 +0100

    Added margin-bottom to footer

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,8 +6,8 @@ import "./WeatherInfo.css";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
       <FriendlyDate date={props.data.date} />
+      <div className="city-name">{props.data.city}</div>
       <WeatherTemperature celsius={props.data.temperature} />
       <p>Humidity: {props.data.humidity}%</p>
       <p>Wind: {props.data.wind} km/h</p>
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celsius");
@@ -19,7 +20,7 @@ export default function WeatherTemperature(props) {
   if (unit === "celsius") {
     return (
       <div className="WeatherTemperature">
-        <span clasName="temperature">{Math.round(props.celsius)}</span>
+        <span className="temperature">{Math.round(props.celsius)} </span>
         <span className="unit">
           ºC |{" "}
           <a href="/" onClick={showFahrenheit}>
@@ -31,7 +32,7 @@ export default function WeatherTemperature(props) {
   } else {
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="temperature">{Math.round(fahrenheit())} </span>
         <span className="unit">
           <a href="/" onClick={showCelsius}>
             °C
